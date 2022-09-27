fdb07abb7b433091695883dd045f6f52e2c9c90a
engelberthRetamal <engel.retamal.e@gmail.com>
2022-09-27 19:00:23 -0300

    Se agregó componente Carousel

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,10 +1,11 @@
 import { View, Text, Button } from 'react-native'
 import React from 'react'
+import Carousel from '../components/Carousel'
 
 export default function HomeScreen({ navigation }) {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Feed Screen</Text>
-      </View>
+            <Carousel></Carousel>
+        </View>
     )
 }
