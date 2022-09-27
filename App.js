fc8ffc2f96e071bc6f25c56d8fc46182f4f9f3d0
engelberthRetamal <engel.retamal.e@gmail.com>
2022-09-27 12:28:32 -0300

    Se agregó drawerNavigator y Provider con la store

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View} from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './src/features/store'
+import { NavigationContainer } from '@react-navigation/native';
+import DrawerNavigator from './src/navigation/DrawerNav'
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+        <NavigationContainer>
+          <DrawerNavigator/>
+        </NavigationContainer>
+    </Provider>
   );
 }
 
