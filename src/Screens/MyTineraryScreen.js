4246ce6d1851ef54e19acd118f103c5b9eb5be59
engelberthRetamal <engel.retamal.e@gmail.com>
2022-09-29 14:05:13 -0300

    Se agregaa dinamismo con el usuario logueado y sus itinerarios

--- src/Screens/MyTineraryScreen.js
+++ src/Screens/MyTineraryScreen.js
@@ -1,15 +1,30 @@
-import { View, Text, Button, StyleSheet } from 'react-native'
+import { View, StyleSheet} from 'react-native'
 import React from 'react'
-import { useNavigation } from '@react-navigation/native';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useGetItineraryUserQuery } from '../features/itinerariesSlice';
 import ItineraryCard from '../components/ItineraryCard';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 export default function MyTineraryScreen() {
-    const navigate = useNavigation();
 
-    let { data: itineraries, refetch } = useGetItineraryUserQuery("63240817c885cdee2c6fe3f8")
+    const [user, setUser] = useState()
+
+    const getUser = async () => {
+        try {
+            const savedUser = await AsyncStorage.getItem("loggedUser");
+            let currentUser = JSON.parse(savedUser)
+            setUser(currentUser)
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    
+    useEffect(() => {
+        getUser()
+    }, [])
+
+    let { data: itineraries, refetch } = useGetItineraryUserQuery(user?.user.id)
 
     return (
         <View style={styles.containerMyTinerary}>
@@ -23,6 +38,15 @@ export default function MyTineraryScreen() {
 const styles = StyleSheet.create({
     containerMyTinerary: {
         flex: 1,
-        alignItems:'center'
+        alignItems: 'center'
+    },
+    loginBtn: {
+        width: "80%",
+        borderRadius: 25,
+        height: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 40,
+        backgroundColor: "#FF1493",
     },
 })
