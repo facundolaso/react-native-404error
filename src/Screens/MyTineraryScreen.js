4644aa32a50464f4912c221a966da1234e6a01c5
engelberthRetamal <engel.retamal.e@gmail.com>
2022-09-30 14:16:00 -0300

    Se incorporó recargar vita para ver nuevos itinerarios

--- src/Screens/MyTineraryScreen.js
+++ src/Screens/MyTineraryScreen.js
@@ -1,25 +1,37 @@
-import { View, StyleSheet} from 'react-native'
+import { View, Text, StyleSheet, RefreshControl, ScrollView } from 'react-native'
 import React from 'react'
 import { useEffect, useState } from 'react';
 import { useGetItineraryUserQuery } from '../features/itinerariesSlice';
 import ItineraryCard from '../components/ItineraryCard';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+const wait = (timeout) => {
+    return new Promise(resolve => setTimeout(resolve, timeout));
+}
 
-export default function MyTineraryScreen({route}) {
+export default function MyTineraryScreen({ route }) {
+
+
+    const [refreshing, setRefreshing] = useState(false);
+
+    const onRefresh = React.useCallback(() => {
+        setRefreshing(true);
+        wait(1000).then(() => setRefreshing(false));
+        refetch()
+    }, []);
 
     let isUserLogged = route.params.isLogged
 
     const [logged, setLogged] = React.useState(false);
 
-    useEffect(()=>{
+    useEffect(() => {
         getUser()
         if (isUserLogged) {
             setLogged(true)
-        } else if (!isUserLogged){
+        } else if (!isUserLogged) {
             setLogged(false)
         }
-    },[isUserLogged])
+    }, [isUserLogged])
 
     const [user, setUser] = useState()
 
@@ -32,7 +44,7 @@ export default function MyTineraryScreen({route}) {
             console.log(error);
         }
     };
-    
+
     useEffect(() => {
         getUser()
     }, [])
@@ -40,18 +52,30 @@ export default function MyTineraryScreen({route}) {
     let { data: itineraries, refetch } = useGetItineraryUserQuery(user?.user.id)
 
     return (
-        <View style={styles.containerMyTinerary}>
-            <View>
-                <ItineraryCard search={itineraries} refetchAction={refetch} isLogged={isUserLogged}/>
-            </View>
-        </View>
+        <ScrollView style={styles.containerMyTinerary} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
+            {itineraries ?
+                <>
+                    {itineraries.response.length > 0 ?
+                        <View>
+                            <ItineraryCard search={itineraries} refetchAction={refetch} isLogged={isUserLogged} />
+                        </View>
+                        :
+                        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                            <Text style={{ fontSize: 40 }}>Swipe Down</Text>
+                        </View>
+                    }
+                </>
+                :
+                null
+            }
+
+        </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
     containerMyTinerary: {
         flex: 1,
-        alignItems: 'center'
     },
     loginBtn: {
         width: "80%",
